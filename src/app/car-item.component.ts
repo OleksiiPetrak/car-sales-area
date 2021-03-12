bb9d28035385b19862f1beff1f613ba5ef05fe1e
OleksiiPetrak <oleksii_petrak@epam.com>
2021-03-12 11:49:08 +0200

    Car collection output provided

--- src/app/car-item.component.ts
+++ src/app/car-item.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Car } from './models/car';
+import { Collection } from './models/collection';
 
 @Component({
     selector: 'car-item',
@@ -10,11 +11,11 @@ import { Car } from './models/car';
 
 export class CarItemComponent implements OnInit {
 
-    car: Car;
+    carItems: Collection<Car>;
 
     constructor(private http: HttpClient){}
 
     ngOnInit(){
-        this.http.get('http://localhost/car/3').subscribe((data:Car)=>this.car=data);
+        this.http.get('http://localhost/car/cars').subscribe((data:Collection<Car>)=>this.carItems=data);
     }
 }
